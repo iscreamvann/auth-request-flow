88cc868c1b9e0fe791db0254885c7f238769969c
Kyle Vann <kyle.vann1996@icloud.com>
2024-07-11 11:21:21 +0100

    complete

--- src/router.js
+++ src/router.js
@@ -14,11 +14,26 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
-
+    const {username, password} = req.body
+    if(username === mockUser.username && password === mockUser.password){
+        const token = jwt.sign({username: mockUser.username}, process.env.JWT_SECRET);
+        res.status(200).json({ jwt: token})
+    }
+    else{
+        res.status(400).json({error: "invalid username and password"})
+    }
 });
 
 router.get('/profile', (req, res) => {
-  
+    jwt.verify(req.headers.authorization, process.env.JWT_SECRET, (error, decoded) => {
+        console.log("decoded", decoded, error)
+        if(error == null){
+            res.status(200).json(mockUser.profile)
+        }
+        else{
+            res.json(400).json({error: "Token invalid"})
+        }
+    })
 });
 
 
